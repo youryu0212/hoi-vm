55c786807dca879daca5015636c8292a0f7996a4
youryu0212 <youryu0212@naver.com>
2022-05-12 11:01:03 +0900

    .

--- src/Components/MoneyScreenBoard/CashReturnButton.styled.js
+++
@@ -1,8 +0,0 @@
-import { applyFlex } from 'Helper/utils';
-import styled from 'styled-components';
-
-export const ReturnButton = styled.button`
-  ${({ theme }) => theme};
-  ${({ flex, justify, align }) => applyFlex({ flex, justify, align })};
-  cursor: pointer;
-`;
--- src/Components/ProductScreenBoard.styled.js
+++
@@ -1,9 +0,0 @@
-import { applyFlex } from 'Helper/utils';
-import styled from 'styled-components';
-
-export const ScreenBoard = styled.div`
-  width: 580px;
-  border: 2px solid #3f51b5;
-  box-sizing: border-box;
-  ${({ flex, wrap }) => applyFlex({ flex, wrap })};
-`;
